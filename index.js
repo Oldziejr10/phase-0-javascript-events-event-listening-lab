f4d725306cfa07ad9386cd548a18b04a33a41ced
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-06-07 19:54:46 -0400

    commit tree e96f42d208c700c5e2cf0be6d3fd515390354673

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+
+document.addEventListener('click', addingEventListener); 
+
 function addingEventListener() {
+    const labTest = input.document.getElementById('input').alert = "I was clicked";
 }
+
